eaa70ab0f806cd1f9c8fb75d516b9a4324a87cd6
tesla707 <tesla707@mail.ru>
2017-03-21 16:59:50 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include "Macros.h"
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
+
+	RenderImage RdrImg;
+
+	return 0;
+}
